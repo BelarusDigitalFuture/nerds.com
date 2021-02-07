18407ed75474fa266fe924493bdeb4a3d8c88ea0
Jan <dev2@mediatool.tv>
2021-02-08 00:52:27 +0200

    added subjects list and routes

--- client/src/components/Home/components/index.jsx
+++ client/src/components/Home/components/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { BellOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 import {
   PageHeader, Button, Descriptions,
   Layout, Row, Col,
 } from 'antd';
+
+import SUBJECTS from 'consts/subjects';
 
 import './styles.scss';
 
@@ -14,7 +16,19 @@ const Home = (props) => {
   return (
     <Row align="center">
       <Col xxl={16} xl={6} lg={8} sm={10} xs={18}>
-        <div className="main">test123</div>
+        <div className="main">
+          <Row align="center" gutter={[16, 16]}>
+            {SUBJECTS.map((item, index) => (
+              <Col key={index}>
+                <Link to={item.path}>
+                  <Button type="primary" shape="round" size="large">
+                    {item.name}
+                  </Button>
+                </Link>
+              </Col>
+            ))}
+          </Row>
+        </div>
       </Col>
     </Row>
   );
