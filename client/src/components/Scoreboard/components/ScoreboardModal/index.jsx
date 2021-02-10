78f4f34888094312dc02b9acb23eadf4fb9325dd
Jan <dev2@mediatool.tv>
2021-02-11 01:10:51 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ client/src/components/Scoreboard/components/ScoreboardModal/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Modal, Button, Space } from 'antd';
+
+const ScoreboardModal = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ScoreboardModal.propTypes = {
+
+}
+
+export default ScoreboardModal
